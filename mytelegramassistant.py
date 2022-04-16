36639ac01f3dc8b98bd72e4311e992d101e63f65
mahdi goodarzi <73882488+mahdigoodarzi123@users.noreply.github.com>
2022-04-16 08:18:50 +0430

    Add files via upload

---
+++ mytelegramassistant.py
@@ -0,0 +1,30 @@
+from pyrogram import Client, filters
+
+
+bot = Client (
+    "erfan's assistant", 
+    api_id= your api id,
+    api_hash= "your api hash",
+    bot_token= "your bot token from botfather"
+)
+
+
+@bot.on_message(filters.text & filters.group)
+def caller_in_gp(client, message):
+    msg = str(message.text)
+    msg2 = msg.split()
+    for item in msg2:
+        if item == "@your username" or item == "@your another username":
+            message.reply_text("sorry (your name) is not available, pls try later")
+
+        elif item == "your name" or item == "your name with capital letters":
+            message.reply_text("BEWARE, (your name) IS EVERYWHERE!")
+
+
+
+@bot.on_message(filters.text & filters.private)
+def message_response(client , message):
+    message.reply_text("hello, (your name) is not available right now.pls try later.")
+
+
+bot.run()
